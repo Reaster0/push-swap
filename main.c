87e86f0b292216400e1ccc8a2671cca3278b3728
Evan Reaster <reaster@MacBook-Reaster.local>
2021-05-31 05:01:54 +0200

    add functions to setup stacks

--- main.c
+++ main.c
@@ -6,21 +6,88 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 02:17:21 by earnaud           #+#    #+#             */
-/*   Updated: 2021/05/31 03:00:37 by earnaud          ###   ########.fr       */
+/*   Updated: 2021/05/31 05:00:00 by earnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	nbr;
+	int	neg;
+
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	neg = (str[i] == '-') * -2 + 1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	nbr = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	return (nbr * neg);
+}
+
+int	error(int ret)
+{
+	write(STDOUT_FILENO ,"Error\n", 6);
+	return (ret);
+}
+
+void	*ft_bzero(void *str, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		((char *)str)[i] = 0;
+		i++;
+	}
+	return (str);
+}
+
+t_stacks	*convert_stack(int argc, char **argv)
+{
+	t_stacks	*stack;
+	int			i;
+
+	i = 0;
+	//stack->a = ft_bzero(malloc(sizeof(int) * (argc -1)), argc -1);
+	//stack->b = ft_bzero(malloc(sizeof(int) * (argc -1)), argc -1);
+	stack->a = malloc(sizeof(int) * (argc -1));
+	stack->b = malloc(sizeof(int) * (argc -1));
+	stack->size = argc -1;
+	argv++;
+	while(*argv)
+	{
+		stack->a[i] = ft_atoi(*argv);
+		argv++;
+		i++;
+	}
+	return (stack);
+}
+
+int	check_error(int argc, char **argv)
+{
+	if (!argc)
+		return (1);
+	//check argv
+	return (0);
+}
+
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	t_stacks stacks;
+
+	t_stacks *stacks;
 	
-
-	stacks.a = malloc(sizeof(int) * stacks.size);
-	stacks.b = malloc(sizeof(int) * stacks.size);
-
+	if (check_error(argc, argv))
+		return (0);
+	stacks = convert_stack(argc, argv);
 	return (0);
 }
--- operations.c
+++ operations.c
@@ -6,124 +6,124 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 02:56:25 by earnaud           #+#    #+#             */
-/*   Updated: 2021/05/31 04:00:06 by earnaud          ###   ########.fr       */
+/*   Updated: 2021/05/31 04:56:20 by earnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void switch_rr(t_stacks stack)
+void switch_rr(t_stacks *stack)
 {
 	switch_ra(stack);
 	switch_rb(stack);
 }
 
-void switch_rb(t_stacks stack)
+void switch_rb(t_stacks *stack)
 {
 	int i;
 	int temp;
 
 	i = 0;
-	temp = stack.b[stack_nb(stack.b)];
+	temp = stack->b[stack_nb(stack->b)];
 	while(i)
 	{
-		stack.b[i] = stack.b[i - 1];
+		stack->b[i] = stack->b[i - 1];
 		i--;
 	}
-	stack.b[i] = temp;
+	stack->b[i] = temp;
 }
 
-void switch_ra(t_stacks stack)
+void switch_ra(t_stacks *stack)
 {
 	int i;
 	int temp;
 
 	i = 0;
-	temp = stack.a[stack_nb(stack.a)];
+	temp = stack->a[stack_nb(stack->a)];
 	while(i)
 	{
-		stack.a[i] = stack.a[i - 1];
+		stack->a[i] = stack->a[i - 1];
 		i--;
 	}
-	stack.a[i] = temp;
+	stack->a[i] = temp;
 }
 
-void switch_rrr(t_stacks stack)
+void switch_rrr(t_stacks *stack)
 {
 	switch_rra(stack);
 	switch_rrb(stack);
 }
 
-void switch_rrb(t_stacks stack)
+void switch_rrb(t_stacks *stack)
 {
 	int i;
 	int temp;
 
 	i = 0;
-	temp = stack.b[0];
-	while(i < stack_nb(stack.b))
+	temp = stack->b[0];
+	while(i < stack_nb(stack->b))
 	{
-		stack.b[i] = stack.b[i + 1];
+		stack->b[i] = stack->b[i + 1];
 		i++;
 	}
-	stack.b[i] = temp;
+	stack->b[i] = temp;
 }
 
-void switch_rra(t_stacks stack)
+void switch_rra(t_stacks *stack)
 {
 	int i;
 	int temp;
 
 	i = 0;
-	temp = stack.a[0];
-	while(i < stack_nb(stack.a))
+	temp = stack->a[0];
+	while(i < stack_nb(stack->a))
 	{
-		stack.a[i] = stack.a[i + 1];
+		stack->a[i] = stack->a[i + 1];
 		i++;
 	}
-	stack.a[i] = temp;
+	stack->a[i] = temp;
 }
 
-void switch_pb(t_stacks stack)
+void switch_pb(t_stacks *stack)
 {
-		if (!stack_nb(stack.a))
+		if (!stack_nb(stack->a))
 		return ;
-	stack.b[stack_nb(stack.b) + 1] = stack.a[stack_nb(stack.a)];
-	stack.a[stack_nb(stack.a)] = 0;
+	stack->b[stack_nb(stack->b) + 1] = stack->a[stack_nb(stack->a)];
+	stack->a[stack_nb(stack->a)] = 0;
 }
 
-void switch_pa(t_stacks stack)
+void switch_pa(t_stacks *stack)
 {
-	if (!stack_nb(stack.b))
+	if (!stack_nb(stack->b))
 		return ;
-	stack.a[stack_nb(stack.a) + 1] = stack.b[stack_nb(stack.b)];
-	stack.b[stack_nb(stack.b)] = 0;
+	stack->a[stack_nb(stack->a) + 1] = stack->b[stack_nb(stack->b)];
+	stack->b[stack_nb(stack->b)] = 0;
 }
 
-void switch_ss(t_stacks stack)
+void switch_ss(t_stacks *stack)
 {
 	switch_sa(stack);
 	switch_sb(stack);
 }
 
-void switch_sb(t_stacks stack)
+void switch_sb(t_stacks *stack)
 {
 	int temp;
-	if (stack_nb(stack.b) <= 1)
+	if (stack_nb(stack->b) <= 1)
 		return ;
-	temp = stack_nb(stack.b);
-	stack.b[stack_nb(stack.b)] = stack.b[stack_nb(stack.b) -1];
-	stack.b[stack_nb(stack.b) -1] = temp; 
+	temp = stack_nb(stack->b);
+	stack->b[stack_nb(stack->b)] = stack->b[stack_nb(stack->b) -1];
+	stack->b[stack_nb(stack->b) -1] = temp; 
 }
 
-void switch_sa(t_stacks stack)
+void switch_sa(t_stacks *stack)
 {
 	int temp;
-	if (stack_nb(stack.a) <= 1)
+	if (stack_nb(stack->a) <= 1)
 		return ;
-	temp = stack.a[stack.size];
-	stack.a[stack.size] = stack.a[stack.size -1];
-	stack.a[stack.size -1] = temp; 
+	temp = stack->a[stack->size];
+	stack->a[stack->size] = stack->a[stack->size -1];
+	stack->a[stack->size -1] = temp; 
 }
 
 int	stack_nb(int *stack)
